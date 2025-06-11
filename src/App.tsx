5cbc9fb32a80e9cb65f19a3510f8054acd46cde4
Raja-Simform <raja.jha@simformsolutions.com>
2025-06-11 14:18:59 +0530

    refactor :use comonent in parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-// import DataTable from "./components/DataTable/DataTable";
+import DataTable from "./components/DataTable/DataTable";
 import FileUploader from "./components/FileUploader/FileUploader";
 
 function App() {
   return (
     <>
-      {/* <DataTable
+      <DataTable
         columns={["Name", "Age", "City", "Occupation", "Joined Date", "Active"]}
         data={[
           ["John Doe", 28, "New York", "Software Engineer", "2020-01-15", true],
@@ -45,7 +45,7 @@ function App() {
           ],
           ["Laura Black", 40, "Denver", "HR Manager", "2016-12-01", true],
         ]}
-      /> */}
+      />
       <FileUploader
         accept=".jpg,.png"
         maxSize={5 * 1024 * 1024}
